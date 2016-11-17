90947976c65ec16e60689745bdff7f55296425ac
byron wall <byron@byroni.us>
2016-11-17 08:27:12 -0700

    Formatting and border code
    Finds the border piece with the lowest strength.  Now need to chart a path to that piece from all the known pieces.

--- HaliteBotCode.py
+++ HaliteBotCode.py
@@ -1,27 +1,49 @@
 from hlt import *
 import logging
 
+
 class HaliteBotCode:
     def __init__(self, map: GameMap):
         self.map = map
-    def update(self, map:GameMap):
+
+    def update(self, map: GameMap):
         self.map = map
-        #add some other code here as well to update the model
-        #TODO check if the map is updated automatically by ref
-        #this will called each frame
+        # add some other code here as well to update the model
+        # TODO check if the map is updated automatically by ref
+        # this will called each frame
         self.updateSiteTarget()
 
         return
-    def setMyId(self, id:int):
+
+    def setMyId(self, id: int):
         self.id = id
         return
+
     def updateSiteTarget(self):
-        #this will go through spaces and find those that are accessible and then ranked by strength
-        self.getBorderOfCurrentSpaces()
+        # this will go through spaces and find those that are accessible and then ranked by strength
+        borderSites = self.getBorderOfCurrentSpaces()
+
+        # actual max is 255
+        minStrength = 256
+        minLocation = None
+
+        for (x, y) in borderSites:
+            location = Location(x, y)
+            site = self.map.getSite(location)
+
+            if site.strength < minStrength:
+                minLocation = location
+                minStrength = site.strength
+
+        logging.debug("minimum site located at %d,%d" % (minLocation.x, minLocation.y))
+
+        # know where to go, need to figure out how/when to get there
+
         return
-    def getBorderOfCurrentSpaces(self):
-        #this will determine the spaces that border the owned pieces
-        #iterate through the spaces
+
+    def getBorderOfCurrentSpaces(self) -> set:
+        # this will determine the spaces that border the owned pieces
+        # iterate through the spaces
 
         borderSites = set()
 
@@ -36,16 +58,16 @@ class HaliteBotCode:
                 site = self.map.getSite(location)
                 doneWithSite = False
 
-                #skip if already found or if owned by self
-                if (x,y) in borderSites or site.owner == self.id:
+                # skip if already found or if owned by self
+                if (x, y) in borderSites or site.owner == self.id:
                     if site.owner == self.id:
                         mapStr += "#"
                     continue
 
                 for direction in DIRECTIONS:
-                    #if a neighbor is owned by self, this is a neighbor, add to set
+                    # if a neighbor is owned by self, this is a neighbor, add to set
                     if self.map.getSite(location, direction).owner == self.id:
-                        borderSites.add((x,y))
+                        borderSites.add((x, y))
                         mapChar = "*"
                         doneWithSite = True
                         break
@@ -54,5 +76,5 @@ class HaliteBotCode:
             mapStr += "\n"
         logging.debug(mapStr)
 
-        #do something with the border sites
+        # do something with the border sites
         return borderSites
