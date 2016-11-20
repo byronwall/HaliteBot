e5513dcef6379b7eb14b1d7d43c1f2cc2c9c38f2
byron wall <byron@byroni.us>
2016-11-19 19:45:44 -0700

    Add a some checking for remote runs

--- MyBot.py
+++ MyBot.py
@@ -1,3 +1,5 @@
+import os
+
 from hlt import *
 from networking import *
 import logging
@@ -9,8 +11,17 @@ haliteBot = HaliteBotCode(gameMap)
 haliteBot.setMyId(myID)
 sendInit("byronwall-halite-1")
 
+if not os.path.isdir("logs"):
+    os.mkdir("logs")
+
 LOG_FILENAME = str(int(time.time())) + "-" + str(myID) + '.log'
 logging.basicConfig(filename="logs/" + LOG_FILENAME, level=logging.DEBUG)
+
+#disbale logging when running on the server
+user_name = os.environ.get("USER")
+if user_name != "byronwall":
+    logging.disable(logging.CRITICAL)
+
 logging.debug('This message should go to the log file')
 
 while True:
