6a3cab8a41888dab629f2b6c8f04edeb6e6b24fa
byron wall <byron@byroni.us>
2016-12-18 15:20:56 -0700

    Update the logic to limit moves
    This adds a check to keep pieces from moving too much.  Adds a "core" set of pieces that will not move if too far from the edge.

--- HaliteBotCode.py
+++ HaliteBotCode.py
@@ -1,17 +1,15 @@
 import random
 from typing import Dict
 from typing import List
-
+from typing import Set
 from hlt import *
 import logging
-from typing import Set
-
 
 class HaliteBotCode:
     def __init__(self, game_map: GameMap, id: int):
         self.id = id
         self.game_map = game_map
-        self.owned_sites = set()
+        self.owned_sites = set() # type: Set[Square]
         self.moves_this_frame = []
 
     def update(self, game_map: GameMap):
@@ -21,16 +19,16 @@ class HaliteBotCode:
         self.moves_this_frame = []
 
         logging.debug("update owned sites")
-        self.updateListOfOwnedSites()
+        self.update_owned_sites()
 
         logging.debug("update the moves")
-        self.updateSiteTarget()
+        self.update_move_targets()
 
         return
 
-    def updateSiteTarget(self):
+    def update_move_targets(self):
         # this will go through spaces and find those that are accessible and then ranked by strength
-        border_sites = self.getBorderOfCurrentSpaces()
+        border_sites = self.get_unowned_border()
 
         # create a dict to assoc border sites
         border_assoc = dict()  # type: Dict[Square, List[Square]]
@@ -41,10 +39,16 @@ class HaliteBotCode:
         # loop through owned pieces and make the calls to move them
         for location in self.owned_sites:
 
+            # these are the check to not make moves
+            if location.strength < location.production * 10:
+                continue
+
             # find the closest border spot
             min_distance = 1000
             min_location = None
             min_strength = 256
+
+            zero_distance = 1000
 
             loc_site = self.game_map.get_target(location)
 
@@ -53,26 +57,26 @@ class HaliteBotCode:
             for border_loc in border_sites:
                 distance = self.game_map.get_distance(border_loc, location)
 
-                # put a threshold on min distance
-                if distance <= 2:
-                    distance = 0
+                border_strength = border_loc.strength
 
-                border_site = self.game_map.get_target(border_loc)
-                border_strength = border_site.strength
-
-                if border_strength == 0:
+                if border_strength == 0 and distance < zero_distance:
                     zero_location = border_loc
+                    zero_distance = distance
                     continue
 
                 if distance < min_distance or (distance == min_distance and border_strength < min_strength):
                     min_distance = distance
                     min_location = border_loc
-                    min_strength = border_site.strength
+                    min_strength = border_loc.strength
 
-                    # know the target border site, add to dict
-                    # allow the zero move if it's the only choice
+            # know the target border site, add to dict
+            # allow the zero move if it's the only choice
             if min_location is None and zero_location is not None:
                 min_location = zero_location
+
+            # add a check here to see if move should be made
+            if min_distance > 4:
+                continue
 
             if min_location is not None and loc_site.strength > 0:
                 border_assoc[min_location].append(location)
@@ -84,16 +88,14 @@ class HaliteBotCode:
             # get the sum of the strengths
             total_strength = 0  # type: int
             for location in locations:
-                total_strength += self.game_map.get_target(location).strength
-
-            border_site = self.game_map.get_target(border_loc)
+                total_strength += location.strength
 
             # this random is a step to allow for making a move anyways
-            if total_strength > border_site.strength or random.random() < 0.05:
+            if total_strength > border_loc.strength:
                 # if so, move that direction
 
                 for location in locations:
-                    move = self.getNextMove(location, border_loc)
+                    move = self.get_next_move(location, border_loc)
 
                     logging.debug("move to make %s", move)
 
@@ -102,7 +104,7 @@ class HaliteBotCode:
 
         return
 
-    def getNextMove(self, start: Square, target: Square) -> Move:
+    def get_next_move(self, start: Square, target: Square) -> Move:
 
         # this will figure out how to get to the desired location, moving only through owned territory
 
@@ -148,7 +150,7 @@ class HaliteBotCode:
 
         return None
 
-    def updateListOfOwnedSites(self):
+    def update_owned_sites(self):
 
         self.owned_sites = list()
 
@@ -160,35 +162,7 @@ class HaliteBotCode:
 
         return
 
-    def getFramesForStrength(self, strength: int) -> int:
-        # this will determine how far out in time is needed to have a total movable strength
-
-        prod_total = 0
-        strength_available = 0
-
-        # get a sum of all the production
-        for location in self.owned_sites:
-            logging.debug("own that site")
-            site = self.game_map.get_target(location)
-            prod_total += site.production
-
-            logging.debug("owned site at %s", location)
-            logging.debug("strengthAvail = %d" % strength_available)
-            logging.debug("strength = %d" % site.strength)
-
-            strength_available += site.strength
-
-        logging.debug("info for strength needed %d %d %d" % (strength, strength_available, prod_total))
-
-        # divide that number by strength desired
-        if prod_total == 0:
-            return 100
-
-        frames = (strength - strength_available) / prod_total
-
-        return 0 if frames < 0 else frames + 1
-
-    def getBorderOfCurrentSpaces(self) -> Set[Square]:
+    def get_unowned_border(self) -> Set[Square]:
         # this will determine the spaces that border the owned pieces
         # iterate through the spaces
 
@@ -199,7 +173,7 @@ class HaliteBotCode:
         for location in self.game_map:
 
             # skip if already found or if owned by self
-            if (location.x, location.y) in border_sites or location.owner == self.id:
+            if location in border_sites or location.owner == self.id:
                 if location.owner == self.id:
                     logging.debug("border found owned site at %s", location)
 
